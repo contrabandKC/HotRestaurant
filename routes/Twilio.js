58862e5620bf33c1d85425ba244d9a6ef7a39cfe
erikmarquez <erikmar3@gmail.com>
2021-07-24 01:43:47 -0500

    twilio Update

--- routes/Twilio.js
+++ routes/Twilio.js
@@ -4,9 +4,6 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 
-// const accountSid = "ACb732612889d7f028289f95cb97cc20d8";
-// const authToken = "52ef413b09e60b91b3f77af6eeed6ea4";
-
 const sendSms = (phone, message) => {
   const client = require('twilio')(accountSid, authToken);
   client.messages
